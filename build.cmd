05265687b9d52a594a84ab44ae9a8d8bbe72da01
shiyixi <3382705185@qq.com>
2025-07-14 18:00:50 +0800

    update(build): 补充

--- build.cmd
+++ build.cmd
@@ -1,3 +1,7 @@
+docker rmi greaterwms:backend
+docker rmi greaterwms:front
+docker rmi greaterwms:front-2
+
 docker build -t greaterwms:backend --target backend .
 docker build -t greaterwms:front --target front .
 docker build -t greaterwms:front-2 --target front-2 .
--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+chmod +x web_start.sh
+chmod +x web_start-2.sh
+chmod +x backend_start.sh
+
+docker rmi greaterwms:backend
+docker rmi greaterwms:front
+docker rmi greaterwms:front-2
+
 docker build -t greaterwms:backend --target backend .
 docker build -t greaterwms:front --target front .
 docker build -t greaterwms:front-2 --target front-2 .
