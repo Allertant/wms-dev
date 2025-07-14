f19422db05c746050a22959a205fc9b754b7849b
shiyixi <3382705185@qq.com>
2025-07-14 17:36:13 +0800

    update(build.sh)：更新 build 构建脚本

--- build.cmd
+++ build.cmd
@@ -1,2 +1,3 @@
 docker build -t greaterwms:backend --target backend .
 docker build -t greaterwms:front --target front .
+docker build -t greaterwms:front-2 --target front-2 .
--- build.sh
+++ build.sh
@@ -2,3 +2,4 @@
 
 docker build -t greaterwms:backend --target backend .
 docker build -t greaterwms:front --target front .
+docker build -t greaterwms:front-2 --target front-2 .
